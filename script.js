20472d837c432c793d4938706db100285c7da0a8
AmeyPank <pankantiameya@.com>
2023-04-01 10:29:11 +0530

    minutedegrees

--- script.js
+++ script.js
@@ -2,22 +2,6 @@ const hourHand = document.querySelector('#hour');
 const minuteHand = document.querySelector('#minute');
 const secondHand = document.querySelector('#second');
 
-// function setDate() {
-//   const now = new Date();
-//   const seconds = now.getSeconds();
-//   const secondsDegrees = ((seconds / 60) * 360) + 90;
-//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-//   const minutes = now.getMinutes();
-//   const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-//   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
-//   const hours = now.getHours();
-//   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
-//   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-// }
-
-// setInterval(setDate, 1000);
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -26,7 +10,7 @@ function setDate() {
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg) matrix(0.737277, -0.67559, 0.67559, 0.737277, 0, 0)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
@@ -34,3 +18,19 @@ function setDate() {
 }
 
 setInterval(setDate, 1000);
+// function setDate() {
+//   const now = new Date();
+//   const seconds = now.getSeconds();
+//   const secondsDegrees = ((seconds / 60) * 360) + 90;
+//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+//   const minutes = now.getMinutes();
+//   const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+//   minuteHand.style.transform = `rotate(${minutesDegrees}deg) matrix(0.737277, -0.67559, 0.67559, 0.737277, 0, 0)`;
+
+//   const hours = now.getHours();
+//   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+//   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+// }
+
+// setInterval(setDate, 1000);
