5c6b6bd64752f20db9f4f9dd34134fa7127f4dd6
AmeyPank <pankantiameya@.com>
2023-04-01 10:06:52 +0530

    JS+CSS Clock Ready to deploy

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-//your code here
-function clock() {
-    const fullDate = new Date();
-    var hours = fullDate.getHours();
-    var mins = fullDate.getMinutes();
-    var secs = fullDate.getSeconds();
-  
-    if (hours < 10) {
-      hours = "0" + hours;
-    }
-    if (mins < 10) {
-      mins = "0" + mins;
-    }
-    if (secs < 10) {
-      secs = "0" + secs;
-    }
-  
-    document.getElementById("hour").innerHTML = hours;
-    document.getElementById("minute").innerHTML = ":" + mins;
-    document.getElementById("second").innerHTML = ":" + secs;
-  }
-  setInterval(clock, 1000);
+const hourHand = document.querySelector('#hour');
+const minuteHand = document.querySelector('#minute');
+const secondHand = document.querySelector('#second');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
