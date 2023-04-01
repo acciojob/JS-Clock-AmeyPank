a1de83ae6754cfd356ce455455c383b7a86284c3
AmeyPank <pankantiameya@.com>
2023-04-01 10:20:02 +0530

    JS+CSS Cloc

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const hourHand = document.querySelector('#hour');
 const minuteHand = document.querySelector('#minute');
 const secondHand = document.querySelector('#second');
 
+// function setDate() {
+//   const now = new Date();
+//   const seconds = now.getSeconds();
+//   const secondsDegrees = ((seconds / 60) * 360) + 90;
+//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+//   const minutes = now.getMinutes();
+//   const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+//   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+//   const hours = now.getHours();
+//   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+//   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+// }
+
+// setInterval(setDate, 1000);
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -10,7 +26,7 @@ function setDate() {
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg) matrix(0.737277, -0.67559, 0.67559, 0.737277, 0, 0)`;
 
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
